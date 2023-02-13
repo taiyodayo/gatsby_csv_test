b74d4081e241a7b4ff0e3e6de87c2418f6094339
Taiyo Nakashima <taiyodayo@gmail.com>
2023-02-13 12:02:51 +0900

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `gatsby_csv_test`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
